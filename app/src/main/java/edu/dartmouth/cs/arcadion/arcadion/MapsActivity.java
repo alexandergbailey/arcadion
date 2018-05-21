02a80959d6004297f57c5990ffd4a7f87bdeb938
Alexander Bailey <alexander.g.w.bailey.18@dartmouth.edu>
2018-05-21 07:40:40 -0400

    events clickable

--- app/src/main/java/edu/dartmouth/cs/arcadion/arcadion/MapsActivity.java
+++ app/src/main/java/edu/dartmouth/cs/arcadion/arcadion/MapsActivity.java
@@ -2,6 +2,8 @@ package edu.dartmouth.cs.arcadion.arcadion;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,14 +14,34 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 import edu.dartmouth.cs.arcadion.arcadion.Login.SignInActivity;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
-    private static final String TAG = "MAPSACTIVITY";
+    private static final String TAG = "MapsActivity";
     private GoogleMap mMap;
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+    private DatabaseReference mDatabase;
+    private Map<Marker, ArrayList<String>> markers = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,14 +60,72 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        // Add a marker in Sydney and move the camera
+        mMap.setOnMarkerClickListener(this);
+        // Get user location and animate camera there
         LatLng collis = new LatLng(43.702778, -72.289849);
-        mMap.addMarker(new MarkerOptions().position(collis).title("Collis Free Food Table"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(collis));
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(collis,17));
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        // Read from the database
+        mDatabase.orderByKey().addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                Log.d(TAG, "LISTENING");
+                Object value = dataSnapshot.getValue(Object.class);
+                if (value == null) {
+                    return;
+                }
+                HashMap<String, ArrayList<String>> list = (HashMap<String, ArrayList<String>>) value;
+                for (String key: list.keySet()) {
+                    Log.d(TAG, key);
+                    Log.d(TAG, list.get(key).toString());
+                    if (list.get(key) == null) {
+                        continue;
+                    }
+                    displayMarker(list.get(key));
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
     }
 
-    // if 'Register' clicked, go to ProfileActivity
+    public void displayMarker(ArrayList<String> data) {
+        Geocoder geocoder = new Geocoder(this);
+        Log.d(TAG, "YOUR ADDRESS:");
+        try {
+            List<Address> address = geocoder.getFromLocationName(data.get(1), 1);
+            if (address.size() > 0) {
+                Log.d(TAG, String.valueOf(address.get(0).getLatitude()));
+                Log.d(TAG, "that was it bruh");
+                LatLng pos = new LatLng(address.get(0).getLatitude(), address.get(0).getLongitude());
+                Marker newMarker = mMap.addMarker(new MarkerOptions().position(pos).title(data.get(0)));
+                markers.put(newMarker, data);
+            }
+        } catch(IOException e) {
+            Log.d(TAG, "IO Exception");
+        }
+    }
+
+    @Override
+    public boolean onMarkerClick(final Marker marker) {
+        ArrayList<String> data = markers.get(marker);
+        Intent intent = new Intent(MapsActivity.this,
+                ViewEventActivity.class);
+        intent.putExtra("MarkerData",data);
+        startActivity(intent);
+        return true;
+    }
+
+
+        // if 'Register' clicked, go to ProfileActivity
     public void onSigninClicked(View v) {
         Intent intent = new Intent(MapsActivity.this,
                 SignInActivity.class);
--- app/src/main/java/edu/dartmouth/cs/arcadion/arcadion/ViewEventActivity.java
+++ app/src/main/java/edu/dartmouth/cs/arcadion/arcadion/ViewEventActivity.java
@@ -1,7 +1,38 @@
 package edu.dartmouth.cs.arcadion.arcadion;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
 
 public class ViewEventActivity extends AppCompatActivity {
 
+    private static final String TAG = "ViewEventActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_event);
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);  //enable back-arrow
+        }
+        ArrayList<String> data = getIntent().getStringArrayListExtra("MarkerData");
+        Log.d(TAG, data.get(1).toString());
+
+    }
+
+    // go back if back-arrow is pressed
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();    // go back if back-arrow pressed
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
